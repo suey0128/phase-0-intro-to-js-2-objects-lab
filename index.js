5b52a9f1bf95485aa2dd64023e283ae7fe436f6e
suey0128 <syui0128@gmail.com>
2021-05-18 12:15:46 -0700

    commit tree 3b7192f9a7438dcfc5861f128676144d12a4832e

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 // Write your solution in this file!
-const employee = {};
+// const employee = {};
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     return Object.assign({}, employee, {[key]:value});
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     return Object.assign(employee, {[key]:value});
+// }
+
+// function deleteFromEmployeeByKey(employee, key) {
+//     let newEm = {...employee};
+//     delete newEm[key];
+//     return newEm;
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee[key];
+//     return employee;
+// }
+
+
+let employee = {};
+
+employee.name = 'bob';
+employee.streetAddress = '23 street';
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    return Object.assign({}, employee, {[key]:value});
+   return Object.assign({},employee,{[key]:value}); 
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    return Object.assign(employee, {[key]:value});
+    employee[key] = value;
+    return employee;
 }
 
 function deleteFromEmployeeByKey(employee, key) {
-    let newEm = {...employee};
-    delete newEm[key];
-    return newEm;
+    let newEmployee = Object.assign({},employee); 
+    delete newEmployee[key];
+    // access an object key, a string follow '.', .a , a is automatically a string. inside [] on the other hand, it is a variable, unless it is [''] 
+    return newEmployee;
 }
 
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
+function  destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
 }
